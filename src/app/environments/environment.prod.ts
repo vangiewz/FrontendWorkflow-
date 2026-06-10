051d3dceca2627b9176289afe77315c779e8dbff
vangiewz <rekteado@gmail.com>
2026-06-09 23:13:31 -0400

    Fix frontend production API URL to new Azure instance

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api-backend-5axms.azurewebsites.net/api',
+  apiUrl: 'https://workflow-backend-rekte.azurewebsites.net/api',
 };
